1de3f0201cef0df65cc72cff240173cdaecf7f24
Jinyoung <jykim0827@gmail.com>
2022-11-08 23:30:02 +0900

    Added lab01

---
+++ lab01/hello.asm
@@ -0,0 +1,16 @@
+	section .data
+msg db 'Hello, world!', 0x0A ;Hello world string
+len equ $ -msg ;length of msg
+
+	section .text
+	global _start ;must be declared for linker (ld)
+_start:	; entry point
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	int 0x80
+
+	mov eax, 1
+	xor ebx, ebx
+	int 0x80
---
+++ lab01/hello_main.asm
@@ -0,0 +1,16 @@
+	section .data
+msg db 'Hello, world!', 0x0A ;Hello world string
+len equ $ -msg ;length of msg
+
+	section .text
+	global main ;must be declared for linker (ld)
+main:	; entry point
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	int 0x80
+
+	mov eax, 1
+	xor ebx, ebx
+	int 0x80
